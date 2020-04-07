ee19feab537c6680892bfe0493fec4b36552c755
bnm000215 <53283888+bnm000215@users.noreply.github.com>
2020-04-07 18:48:22 +0900

    1.0.0

---
+++ com.nkstudio.buttonpro/Runtime/ButtonGroup.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+
+public class ButtonGroup : MonoBehaviour
+{
+    #region Show Inspector
+
+    [Header("그룹화 되어야하는 버튼들")]
+    [Tooltip("그룹으로 지어줄 버튼들")]
+    public ButtonPro[] buttonPros = new ButtonPro[2];
+
+    [Header("초기에 선택되어 있어야하는 버튼")]
+    [SerializeField, Tooltip("초기 선택될 버튼")]
+    private int _SelectedNumber = -1;
+
+    #endregion
+
+    #region Hide Inspector
+
+    public int SelectedNumber
+    {
+        get => _SelectedNumber;
+        set
+        {
+            _preNumber = _SelectedNumber;
+            _SelectedNumber = value;
+        }
+    }
+
+    //이전에 선택된 버튼입니다.
+    private int _preNumber;
+
+    #endregion
+
+    private void Awake()
+    {
+        //초기화
+        _preNumber = SelectedNumber;
+
+        //등록된 버튼이 1미만이면 : return
+        if (buttonPros.Length < 1)
+            return;
+
+        //전체 오브젝트가 선택되지 않도록하고, 그룹화 한다.
+        foreach (var btnPro in buttonPros)
+        {
+            btnPro.isSelected = false;
+            btnPro._buttonGroup = this;
+        }
+
+        //선택 넘버가 따로 설정되어있지 않다면,
+        if (SelectedNumber == -1)
+            return;
+
+        //선택 넘버가 값이 오버되는 것을 방지
+        SelectedNumber = Mathf.Clamp(SelectedNumber, 0, buttonPros.Length);
+
+        //해당 버튼이 선택되도록 변경.
+        buttonPros[SelectedNumber].isSelected = true;
+    }
+    
+    private void Start()
+    {
+        //선택 넘버가 따로 설정되어있지 않다면 : return
+        if (SelectedNumber == -1)
+            return;
+        
+        buttonPros[SelectedNumber].onSelectButton();
+    }
+
+    /// <summary>
+    /// 해당 버튼이 버튼 그룹에서 몇번째 인덱스인지 반환합니다.
+    /// </summary>
+    /// <param name="Buttons">그룹화된 버튼</param>
+    /// <param name="Button">찾을 버튼</param>
+    public static int ButtonSearch(ButtonPro[] Buttons, ButtonPro Button)
+    {
+        //리턴될 변수
+        int index = -1;
+
+        //해당 버튼 개수만큼 반복
+        for (int i = 0; i < Buttons.Length; i++)
+        {
+            //버튼 그룹중에 해당 버튼이 아니라면 : continue
+            if (Buttons[i] != Button) continue;
+
+            index = i;
+        }
+
+        return index;
+    }
+
+    /// <summary>
+    /// SelectedNumber을 Set하고, 버튼의 렌더링 상태를 변경해야할 때, 호출합니다.
+    /// </summary>
+    public void notifyDataSetChanged()
+    {
+        buttonPros[_preNumber].onNotSelectButton();
+        buttonPros[SelectedNumber].onSelectButton();
+    }
+}
---
+++ com.nkstudio.buttonpro/Runtime/ButtonPro.cs
@@ -0,0 +1,135 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ButtonPro : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler,
+    IPointerUpHandler
+{
+    #region Enum
+
+    private enum BtnState
+    {
+        NORMAL,
+        PRESS
+    }
+
+    #endregion
+
+    #region Show Inspector
+
+    [Header("이미지")]
+    [SerializeField, Tooltip("기본 상태일 때")]
+    private Sprite normalImage;
+
+    [SerializeField, Tooltip("마우스가 닿았을 때")]
+    private Sprite reachImage;
+
+    [SerializeField, Tooltip("눌렸을 때")]
+    private Sprite pressImage;
+
+    [SerializeField, Tooltip("누르고 땟을 때, 함수를 실행합니다.")]
+    private UnityEvent onDownUp;
+
+    #endregion
+
+    #region Hide Inspector
+
+    //이미지 변수
+    private Image image;
+
+    //버튼의 상태에 대한 변수
+    private BtnState _btnState;
+
+    //그룹화가 형태인 버튼인지 아닌지 처리
+    [HideInInspector]
+    public ButtonGroup _buttonGroup;
+
+    //그룹화로 만들시, 선택권이 있는 버튼
+    [HideInInspector]
+    public bool isSelected;
+
+    #endregion
+
+    private void Awake() => //초기화 해줍니다.
+        image = GetComponent<Image>();
+
+    //버튼을 누름
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //왼쪽 마우스를 누른 경우에만 해당
+        if (!Input.GetMouseButton(0)) return;
+
+        //활성화
+        onSelectButton();
+
+        //할당이 되어 있다면, 몇번째 버튼이 선택됬는지 버튼 그룹에게 알려준다.
+        if (_buttonGroup == null) return;
+
+        //현재 선택된 버튼이 자신임을 변경한다.
+        _buttonGroup.SelectedNumber = ButtonGroup.ButtonSearch(_buttonGroup.buttonPros, this);
+
+        //데이터 변경 처리
+        _buttonGroup.notifyDataSetChanged();
+    }
+
+    //버튼을 땜
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //왼쪽 마우스를 누른 경우에만 해당
+        if (!Input.GetMouseButtonDown(0)) return;
+
+        //그룹이 할당이 되어 있지 않을 때, 버튼을 땟을 시 : Not Select
+        if (_buttonGroup == null)
+            onNotSelectButton();
+
+        //연결된 이벤트가 동작되도록 합니다.
+        onDownUp?.Invoke();
+    }
+
+    //버튼 영역에 닿음
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        //선택 상태가 아닐경우 : return
+        if (isSelected) return;
+
+        if (reachImage != null)
+            image.sprite = reachImage;
+    }
+
+    //버튼 영역에서 나감
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //누르고 있는 상태라면 : return
+        if (_btnState == BtnState.PRESS) return;
+
+        //선택된 상태가 아닐 때, 마우스가 UI영역 밖으로 나갔을 시 : 초기화
+        if (!isSelected)
+            onNotSelectButton();
+    }
+
+    public void onSelectButton()
+    {
+        if (pressImage != null)
+            image.sprite = pressImage;
+
+        _btnState = BtnState.PRESS;
+
+        //버튼 그룹이 할당됬을 시 : Select
+        if (_buttonGroup != null)
+            isSelected = true;
+    }
+
+    public void onNotSelectButton()
+    {
+        if (normalImage != null)
+            image.sprite = normalImage;
+
+        _btnState = BtnState.NORMAL;
+
+        //버튼 그룹이 할당됬을 시 : Not Select
+        if (_buttonGroup != null)
+            isSelected = false;
+    }
+}
